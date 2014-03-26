36ccfe59df3942e5e047db8fa38c8ec542257de6
A208 <A208@ISA2080010.sac.rsccd.inst>
2014-03-25 21:38:32 -0700

    Boolean Loops

---
+++ Boolean Loops/src/BooleanLoops.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+import java.util.Random;
+public class BooleanLoops {		
+	public static void main(String [] args){
+			
+			int game = 0 + (int) (Math.random() * 10);		
+			System.out.printf("Welcome to the guess a number game, please type in a number between 0 and 10\n");		
+			
+			for (int a =1; a<4; a++){
+			Scanner input = new Scanner(System.in);		
+			int user = input.nextInt();
+			//Initialize boolean values to false.
+			boolean equal = false;
+			boolean low =  false;
+			boolean high = false;
+			
+			//Comparison between numbers will change one of the boolean values to true.
+			if (user == game){			
+				equal = true;
+			}		
+			else if (user < game){			
+				low = true;
+			}		
+			else if (user > game){			
+				high = true;	
+			}
+			
+			if (equal == true) {			
+				System.out.printf("Congratulations, the number %d is the correct choice. You have won the game.\n", user);
+			}		
+			if (low == true){			
+				System.out.printf("Sorry but the number %d is too low, the correct number is %d, please try again by restarting program.\n", user , game);		
+			}		
+			if (high == true){			
+				System.out.printf("Sorry but the number %d is too high, the correct number is %d, please try again by restarting program.\n", user , game);			
+			}
+		}
+	}
+}
